4831957a637e3640c5f21980db5b7cdee68b183f
Ahmed  Afify <afify@eduroam-int-dhcp-97-62-93.ucl.ac.uk>
2018-07-12 17:26:08 +0100

    mongo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://remotehealthtrackingdb:HREQkyEFCveXM7egDJ9XT31GNbkGJvfHGPdFu9yBw7zJUjHx9UDqy6AADFVUjWU8O18tQYTG0UxC59MTSrWHWw==@remotehealthtrackingdb.documents.azure.com:10255/?ssl=true", function (err, db) {
+  db.close();
+});
+
 module.exports = router;
